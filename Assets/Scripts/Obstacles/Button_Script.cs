656f0d3fa55efacf3a09fc69bc864e09cd98fdbc
MaciàTP <maciatp@gmail.com>
2023-12-25 18:31:52 +0100

    Button Clode Cleaning

--- Assets/Scripts/Obstacles/Button_Script.cs
+++ Assets/Scripts/Obstacles/Button_Script.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Button_Script : MonoBehaviour
-{
-    public bool isButtonUsable = true;
-    public bool isButtonActivated = false;
-    public bool isButtonLockableWhenActivated = false;
-    public MeshRenderer buttonMesh;
-    public ScoreManager_Script scoreManager_Script_;
-    //public BoxCollider buttonCollider;
+{   
+    [SerializeField] bool isButtonActivated = false;
+    [SerializeField] bool isButtonLockableWhenActivated = false;
+    
+    public bool IsButtonActivated
+    {
+        get { return isButtonActivated; }
+        set { isButtonActivated = value; }
+    }
+    public bool IsButtonLockableWhenActivated
+    {
+        get { return isButtonLockableWhenActivated; }
+        set { isButtonLockableWhenActivated = value; }
+    }
+
 
     private void Awake()
-    {
-        scoreManager_Script_ = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager_Script>();
-        //buttonCollider = this.gameObject.GetComponent<BoxCollider>();
-        buttonMesh = this.gameObject.GetComponent<MeshRenderer>();
-        buttonMesh.material.color = Color.red;
+    {   
+        GetComponent<MeshRenderer>().material.color = Color.red;
     }
 
     
@@ -35,29 +40,28 @@ public class Button_Script : MonoBehaviour
         if ((isButtonActivated == false) && ((collision.gameObject.tag == "LaserBeam") || (collision.gameObject.tag == "ChargedLaserSphere") || (collision.gameObject.tag == "Bomb")))
         {
             ActivateButton();
-            Debug.Log("He activado botón");
 
         }
     }
 
     public void ActivateButton()
     {
-        //buttonCollider.enabled = false;
+        Debug.Log("He activado botón");        
         isButtonActivated = true;
-        scoreManager_Script_.AddHits(1);
-        buttonMesh.material.color = Color.green;
-        isButtonUsable = false;
-        if(this.transform.parent.name == "DoorHouse_Button") // SÓLO FUNCIONA SI SE LLAMA EXACTAMENTE IGUAL (CON CLONE INCLUIDO)
+        GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager_Script>().AddHits(1);
+        GetComponent<MeshRenderer>().material.color = Color.green;
+        
+        if(transform.parent.name == "DoorHouse_Button") // SÓLO FUNCIONA SI SE LLAMA EXACTAMENTE IGUAL (CON CLONE INCLUIDO)
         {
-            this.transform.parent.GetComponent<DoorHouse_Script>().OpenDoor();
+            transform.parent.GetComponent<DoorHouse_Script>().OpenDoor();
         }
-        if(this.transform.parent.name == "Column_Button_Prefab")
+        if(transform.parent.name == "Column_Button_Prefab")
         {
-            this.transform.parent.GetComponent<ColumnPrefab_Script>().PlayAnimationColumn();
+            transform.parent.GetComponent<ColumnPrefab_Script>().PlayAnimationColumn();
         }
-        if(this.transform.name == "Button_TrackChanger")
+        if(transform.name == "Button_TrackChanger")
         {
-            this.transform.parent.GetChild(1).gameObject.GetComponent<TrackChanger_Trigger_Script>().EnableTrigger();
+            transform.parent.GetChild(1).gameObject.GetComponent<TrackChanger_Trigger_Script>().EnableTrigger();
         }
     }
 }
