fb93d7701d8293457f2dd67da3b076f32bc0a22a
MaciàTP <maciatp@gmail.com>
2023-12-28 18:29:50 +0100

    Light Trigger Disabled

--- Assets/LightTrigger_Script.cs
+++ Assets/LightTrigger_Script.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class LightTrigger_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -22,26 +12,26 @@ public class LightTrigger_Script : MonoBehaviour
             Debug.Log("Voy a encender la luz");
             Debug.Log(other.gameObject.name);
             //Debug.Log(other.transform.Find("Directional Light").gameObject.name);
-            other.transform.Find("Directional_Light").gameObject.SetActive(true);//.GetComponent<Light>().enabled = true;
+            other.transform.Find("Light").gameObject.SetActive(true);//.GetComponent<Light>().enabled = true;
             Debug.Log("Y se hizo la luz!");
         }
 
         if (other.CompareTag("Wingman"))
         {
             Debug.Log(other.name);
-            other.transform.Find("Directional_Light").gameObject.SetActive(true);
+            other.transform.Find("Light").gameObject.SetActive(true);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            other.transform.Find("Directional_Light").gameObject.SetActive(false); //GetComponent<Light>().enabled = false;
+            other.transform.Find("Light").gameObject.SetActive(false); //GetComponent<Light>().enabled = false;
         }
         if (other.CompareTag("Wingman"))
         {
             Debug.Log(other.name);
-            other.transform.Find("Directional_Light").gameObject.SetActive(false);
+            other.transform.Find("Light").gameObject.SetActive(false);
         }
     }
 }
